b5329efc69e06a1ee873e35ef4e3f8e8256d51bc
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-04-17 17:50:02 +0300

    Created test for Instance of Encrypt

--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -1,5 +1,13 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EncryptTest {
+
+    @Test
+    public void encrypt_createInstanceOfEncrypt_true() throws Exception{
+        Encrypt encrypt=new Encrypt("Hello my name is ruth");
+        assertEquals(true,encrypt instanceof Encrypt);
+    }
 
 }
