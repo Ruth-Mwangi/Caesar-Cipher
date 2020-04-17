d52ad01a143cf9c2c50cbc08aa1dddffe84bf95b
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-04-17 21:04:59 +0300

    Created test for checking if outputed string is correct

--- src/test/java/DecryptTest.java
+++ src/test/java/DecryptTest.java
@@ -5,10 +5,19 @@ import static org.junit.Assert.*;
 public class DecryptTest {
 
     @Test
-    public void newDecrypt_createdInstanceOfDerypt_true() throws Exception{
+    public void newDecrypt_createdInstanceOfDecrypt_true() throws Exception{
         Decrypt decrypt=new Decrypt();
-        String output=decrypt.Decrypt("fcf",2);
+        String output=decrypt.Decrypt("fcd",2);
         assertEquals(true,output instanceof String);
     }
+
+    @Test
+    public void decrypt_decryptMessage_String() throws Exception{
+        Decrypt decrypt=new Decrypt();
+        String expectedOutput="DAD";
+        String output=decrypt.Decrypt("fcf",2);
+        assertEquals(expectedOutput,output);
+    }
+
 
 }
