41727e9c3fd2b2bf63a753888dfc265e4ef65cbf
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-04-17 20:43:31 +0300

    Created private variables for Decrypt class

--- src/main/java/Decrypt.java
+++ src/main/java/Decrypt.java
@@ -1,3 +1,15 @@
 public class Decrypt {
+    private String mOriginalMessage;
+    private String mDecryptedMessage;
+    private int mKey;
+    private char[] mOriginalCharArray;
+    private String mAlphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private int currentIndex;
+    private int newIndex;
+    private char decryptedLetter;
+
+
+
+
 
 }
