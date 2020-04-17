48e8c0bf089b3a773298751158a6a54b9f8ff9cc
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-04-17 18:05:54 +0300

    Added more private variables to the Encrypt class

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -2,9 +2,11 @@ public class Encrypt {
     private String mOriginalMessage;
     private String mEncryptedMessage;
     private int mKey;
+    private char[] result;
+    private String alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     public Encrypt(String mOriginalMessage,int mKey) {
-        this.mOriginalMessage = mOriginalMessage;
+        this.mOriginalMessage = mOriginalMessage.toUpperCase();
         this.mKey=mKey;
     }
 }
