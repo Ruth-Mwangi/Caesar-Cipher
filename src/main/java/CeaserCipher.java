41288e2845800550aa848a7af435c0adbaaf3180
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-04-17 17:19:36 +0300

    Created a constructor for CaeserCipher class which accepts a message and key

--- src/main/java/CeaserCipher.java
+++ src/main/java/CeaserCipher.java
@@ -1,5 +1,9 @@
 public class CeaserCipher {
     private String mMessage;
     private int mKey;
+    public CeaserCipher(String mMessage, int mKey) {
+        this.mMessage = mMessage;
+        this.mKey = mKey;
+    }
 
 }
