13f21b1daadf9f5f380ac5a680446cc190ad4699
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-04-17 17:42:06 +0300

    Created private variables for the Encrypt class

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,2 +1,8 @@
 public class Encrypt {
+    private String mOriginalMessage;
+    private String mEncryptedMessage;
+
+    public Encrypt(String mOriginalMessage) {
+        this.mOriginalMessage = mOriginalMessage;
+    }
 }
