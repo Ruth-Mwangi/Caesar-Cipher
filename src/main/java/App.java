c448b7fd22bfabb7bfbf0d066299f69e6addcb5e
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-04-17 21:23:44 +0300

    Created while loop

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+
+        boolean programRunning=true;
+        while (programRunning){
+            System.out.println("Welcome to the Caesar Cipher");
+            System.out.println("Do you want to Encrypt or Decrypt or Exit");
+            String navigate =scan.nextLine();
+        }
 
     }
 }
