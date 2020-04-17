5d74c5caf2e7810c258d71f1fdc1c80641f5ecd7
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-04-17 18:31:55 +0300

    Added logic to the mEncryptMessage method

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,5 +1,6 @@
 public class Encrypt {
     private String mOriginalMessage;
+    private String mEncryptMessage;
     private int mKey;
     private char[] mOriginalCharArray;
     private String mAlphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -10,18 +11,33 @@ public class Encrypt {
     public Encrypt(String mOriginalMessage,int mKey) {
         this.mOriginalMessage = mOriginalMessage.toUpperCase();
         this.mKey=mKey;
-        mOriginalCharArray=mOriginalMessage.toCharArray();
-        mEncryptMessage(mOriginalCharArray,this.mKey);
+
+        mEncrypt(mOriginalMessage,this.mKey);
 
 
     }
 
-    private String mEncryptMessage(char[] mOriginalCharArray,int mKey){
+    private String mEncrypt(String mOriginalMessage,int mKey){
+
+        mEncryptMessage=mOriginalMessage;
+
+        mOriginalCharArray=mOriginalMessage.toCharArray();
 
         for(char i:mOriginalCharArray){
             currentIndex=mAlphabet.indexOf(i);
             newIndex=(currentIndex+mKey)%26;
-            if(newIndex>26)
+            if(newIndex>26){
+                newIndex-=26;
+                encryptedLetter=mAlphabet.charAt(newIndex);
+                mEncryptMessage.replaceAll(String.valueOf(i),String.valueOf(encryptedLetter));
+
+            }
+            else {
+                encryptedLetter=mAlphabet.charAt(newIndex);
+                mEncryptMessage.replaceAll(String.valueOf(i),String.valueOf(encryptedLetter));
+
+            }
+
 
         }
 
